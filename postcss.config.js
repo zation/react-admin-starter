647a6a3b4f8a768f5beec7bdc5dd4985f4e1bcd2
Liu Yang <zation1@gmail.com>
2017-07-29 17:58:48 +0800

    post css config

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,7 @@
+/* eslint-disable global-require */
+
 module.exports = {
-  plugins: {
-    autoprefixer: {
-      browsers: [
-        'ie_mob >= 9',
-        'ff >= 30',
-        'chrome >= 34',
-        'safari >= 7',
-        'opera >= 23',
-        'ios >= 7',
-        'android >= 2.3',
-        'bb >= 10',
-      ],
-    },
-  },
+  plugins: [
+    require('autoprefixer')(/* package.json/browserslist */),
+  ],
 };
