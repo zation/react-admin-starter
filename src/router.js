704a40d2d88d9d7272d1627f5f5fd240bf9d1fe4
Liu Yang <zation1@gmail.com>
2017-07-29 17:07:58 +0800

    not to request duplicated data

--- src/router.js
+++ src/router.js
@@ -25,6 +25,9 @@ import {
 import {
   readAll as readAllOrders,
 } from 'shared/entities/actions/order';
+import { isEntityEmpty } from 'shared/entities/get-entity';
+import { flow, filter, map } from 'lodash/fp';
+
 import Profile from 'modules/account/containers/profile';
 
 import authRoutes from 'modules/auth/routes';
@@ -33,21 +36,47 @@ import contentRoutes from 'modules/content/routes';
 import accountRoutes from 'modules/account/routes';
 import shopRoutes from 'modules/shop/routes';
 
+const preloadEntities = [{
+  name: 'user',
+  action: readAllUsers,
+}, {
+  name: 'role',
+  action: readAllRoles,
+}, {
+  name: 'content',
+  action: readAllContents,
+}, {
+  name: 'shopBanner',
+  action: readAllShopBanners,
+}, {
+  name: 'productCategory',
+  action: readAllProductCategories,
+}, {
+  name: 'product',
+  action: readAllProducts,
+}, {
+  name: 'coupon',
+  action: readAllCoupons,
+}, {
+  name: 'order',
+  action: readAllOrders,
+}];
+
 export default new Router([
   authRoutes, {
     path: '/',
-    onEnter: async ({ store: { dispatch } }) => {
-      await dispatch(readMineUser());
-      await Promise.all([
-        dispatch(readAllUsers()),
-        dispatch(readAllRoles()),
-        dispatch(readAllContents()),
-        dispatch(readAllShopBanners()),
-        dispatch(readAllProductCategories()),
-        dispatch(readAllProducts()),
-        dispatch(readAllCoupons()),
-        dispatch(readAllOrders()),
-      ]);
+    onEnter: async ({ store: { dispatch, getState } }) => {
+      const state = getState();
+      if (isEntityEmpty('auth.currentUserId')(state)) {
+        await dispatch(readMineUser());
+      }
+
+      await Promise.all(
+        flow(
+          filter(({ name }) => isEntityEmpty(name)(state)),
+          map(({ action }) => dispatch(action())),
+        )(preloadEntities),
+      );
     },
     children: [
       {
--- src/shared/entities/get-entity.js
+++ src/shared/entities/get-entity.js
@@ -1,7 +1,14 @@
-import { prop, curry, flow, map } from 'lodash/fp';
+import { prop, curry, flow, map, isNil, isObject, isEmpty } from 'lodash/fp';
+
+const getEntity = curry((keyPath, state) => prop(`entities.${keyPath}`, state));
 
 export const getEntityArray = curry(
-  (keyPath, store) => flow(prop(`entities[${keyPath}]`), map(value => value))(store),
+  (keyPath, state) => flow(prop(`entities[${keyPath}]`), map(value => value))(state),
 );
 
-export default curry((keyPath, store) => prop(`entities.${keyPath}`, store));
+export const isEntityEmpty = curry((keyPath, state) => {
+  const result = getEntity(keyPath)(state);
+  return isNil(result) || (isObject(result) && isEmpty(result));
+});
+
+export default getEntity;
